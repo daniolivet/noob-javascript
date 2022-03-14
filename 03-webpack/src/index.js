225c78013d9cc842bb42ce2438e1267b86a48fe7
daniolivet <olivetjimenezdaniel@gmail.com>
2022-03-14 17:48:37 +0100

    first steps in webpack

---
+++ 03-webpack/src/index.js
@@ -0,0 +1,5 @@
+import { hello } from './js/components';
+
+const name = 'Daniel';
+
+hello(name);
---
+++ 03-webpack/src/js/components.js
@@ -0,0 +1,8 @@
+export const hello = ( name ) => {
+    console.log('Create tag h1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hello, ${ name }`;
+
+    document.body.append(h1);
+}
